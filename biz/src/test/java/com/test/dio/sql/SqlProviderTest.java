01a1c5016d1339e2d9a58a63114fc779a15fc794
Pumbaa <pengsyhx@gmail.com>
2020-09-25 17:54:16 +0800

    add test

--- biz/src/test/java/com/test/dio/sql/SqlProviderTest.java
+++ biz/src/test/java/com/test/dio/sql/SqlProviderTest.java
@@ -2,6 +2,9 @@ package com.test.dio.sql;
 
 import com.test.dio.biz.config.BizConfig;
 import com.test.dio.biz.util.ModuConfProvider;
+import org.joda.time.LocalDate;
+import org.joda.time.LocalDateTime;
+import org.joda.time.format.DateTimeFormat;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +25,22 @@ public class SqlProviderTest {
     public void testRightJoin() {
         String sql = moduConfProvider.validScriptSql();
         System.out.println(sql);
+    }
+
+    @Test
+    public void testReplace() {
+        String sql = "aaaa < d2d2";
+        String s = sql.replaceAll("<", "&lt;");
+        System.out.println(s);
+    }
+
+    @Test
+    public void testLocalDateTime() {
+        String now = LocalDateTime.now().toString("yyyy-MM-dd HH:mm:00");
+
+
+        LocalDateTime parse = LocalDateTime.parse(now, DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss"));
+
+        System.out.println(parse);
     }
 }
